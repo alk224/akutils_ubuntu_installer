c5d3e67605b4c9760411948f9b36764a838de537
alk224 <alk224@nau.edu>
2015-08-29 16:52:06 -0700

    problem with mirror source still

--- r_updates.r
+++ r_updates.r
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 ## Update R installation with necessary packages
-
+#chooseCRANmirror("http://cran.cnr.berkeley.edu/")
 install.packages(c('ape', 'biom', 'optparse', 'RColorBrewer', 'randomForest', 'vegan'))
 source('http://bioconductor.org/biocLite.R')
 biocLite(c('DESeq2', 'metagenomeSeq'))
