b18889f963b46f4160b29f7d8299679f406439fc
alk224 <alk224@nau.edu>
2015-10-30 09:24:01 -0700

    restructuring

--- scripts/ppa_slave.sh
+++ scripts/ppa_slave.sh
@@ -24,7 +24,7 @@ echo "Adding extra ppas.
 " >> $log
 
 ## System load indicator source
-mlicount1=`grep "indicator-multiload" $scriptdir/ppas/ppa_log.txt  2>/dev/null | wc -l`
+mlicount1=`grep "indicator-multiload" $scriptdir/updates/ppa_log.txt  2>/dev/null | wc -l`
 mlicount2=`ls /etc/apt/sources.list.d/indicator-multiload-stable-daily*  2>/dev/null | wc -l`
 if [[ $mlicount1 == 0 ]]; then
 if [[ $mlicount2 == 0 ]]; then
@@ -32,17 +32,17 @@ apt-add-repository -y ppa:indicator-multiload/stable-daily 1>$stdout 2>$stderr |
 echo "Indicator mutliload ppa:
 Installed on $date0" >> $log
 	bash $scriptdir/scripts/log_slave.sh $stdout $stderr $log
-echo "indicator-multiload" >> $scriptdir/ppas/ppa_log.txt
+echo "indicator-multiload" >> $scriptdir/updates/ppa_log.txt
 a="1"
 else
-echo "indicator-multiload" >> $scriptdir/ppas/ppa_log.txt
+echo "indicator-multiload" >> $scriptdir/updates/ppa_log.txt
 echo "Indicator mutliload ppa:
 Installed previously." >> $log
 fi
 fi
 
 ## Gimp Image Editor source
-ottocount1=`grep "otto-kesselgulasch-gimp" $scriptdir/ppas/ppa_log.txt 2>/dev/null | wc -l`
+ottocount1=`grep "otto-kesselgulasch-gimp" $scriptdir/updates/ppa_log.txt 2>/dev/null | wc -l`
 ottocount2=`ls /etc/apt/sources.list.d/otto-kesselgulasch-gimp*  2>/dev/null | wc -l`
 if [[ $ottocount1 == 0 ]]; then
 if [[ $ottocount2 == 0 ]]; then
@@ -50,17 +50,17 @@ add-apt-repository -y ppa:otto-kesselgulasch/gimp 1>$stdout 2>$stderr || true
 echo "Gimp ppa:
 Installed on $date0" >> $log
 	bash $scriptdir/scripts/log_slave.sh $stdout $stderr $log
-echo "otto-kesselgulasch-gimp" >> $scriptdir/ppas/ppa_log.txt
+echo "otto-kesselgulasch-gimp" >> $scriptdir/updates/ppa_log.txt
 b="1"
 else
-echo "otto-kesselgulasch-gimp" >> $scriptdir/ppas/ppa_log.txt
+echo "otto-kesselgulasch-gimp" >> $scriptdir/updates/ppa_log.txt
 echo "Gimp ppa:
 Installed previously." >> $log
 fi
 fi
 
 ## Cran R source
-rppacount1=`grep "cran.rstudio.com" $scriptdir/ppas/ppa_log.txt 2>/dev/null | wc -l`
+rppacount1=`grep "cran.rstudio.com" $scriptdir/updates/ppa_log.txt 2>/dev/null | wc -l`
 rppacount2=`cat /etc/apt/sources.list | grep "cran.rstudio.com"  2>/dev/null | wc -l`
 if [[ $rppacount1 == 0 ]]; then
 if [[ $rppacount2 == 0 ]]; then
@@ -69,17 +69,17 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9 1>$stdout 2>$s
 echo "R (cran) ppa:
 Installed on $date0" >> $log
 	bash $scriptdir/scripts/log_slave.sh $stdout $stderr $log
-echo "cran.rstudio.com" >> $scriptdir/ppas/ppa_log.txt
+echo "cran.rstudio.com" >> $scriptdir/updates/ppa_log.txt
 c="1"
 else
-echo "cran.rstudio.com" >> $scriptdir/ppas/ppa_log.txt
+echo "cran.rstudio.com" >> $scriptdir/updates/ppa_log.txt
 echo "R (cran) ppa:
 Installed previously." >> $log
 fi
 fi
 
 ## Yppa Manager source
-yppacount1=`grep "webupd8team-y-ppa-manager" $scriptdir/ppas/ppa_log.txt 2>/dev/null | wc -l`
+yppacount1=`grep "webupd8team-y-ppa-manager" $scriptdir/updates/ppa_log.txt 2>/dev/null | wc -l`
 yppacount2=`ls /etc/apt/sources.list.d/webupd8team-y-ppa-manager*  2>/dev/null | wc -l`
 if [[ $yppacount1 == 0 ]]; then
 if [[ $yppacount2 == 0 ]]; then
@@ -87,10 +87,10 @@ add-apt-repository -y ppa:webupd8team/y-ppa-manager 1>$stdout 2>$stderr || true
 echo "Y ppa manager ppa:
 Installed on $date0" >> $log
 	bash $scriptdir/scripts/log_slave.sh $stdout $stderr $log
-echo "webupd8team-y-ppa-manager" >> $scriptdir/ppas/ppa_log.txt
+echo "webupd8team-y-ppa-manager" >> $scriptdir/updates/ppa_log.txt
 d="1"
 else
-echo "webupd8team-y-ppa-manager" >> $scriptdir/ppas/ppa_log.txt
+echo "webupd8team-y-ppa-manager" >> $scriptdir/updates/ppa_log.txt
 echo "Y ppa manager ppa:
 Installed previously." >> $log
 fi
