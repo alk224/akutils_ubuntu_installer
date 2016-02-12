8e87ad398071aa09788351619dbbd96930bd90e4
alk224 <alk224@nau.edu>
2016-02-12 13:55:38 -0700

    fixed date error when considering R updates

--- akutils_ubuntu_installation.sh
+++ akutils_ubuntu_installation.sh
@@ -350,9 +350,9 @@ source /etc/environment 1>$stdout 2>$stderr || true
 
 ## Update R packages if not been done in over a month
 	Rdate0=`head -1 $scriptdir/updates/R_installs_and_updates.txt`
-	Rdate1=`date +%Y,%m,%d`
+	Rdate1=`date +%Y,%m,%-d`
 	span=`python -c "from datetime import date; print (date($Rdate1)-date($Rdate0)).days"`
-	if [[ $span -ge 31 ]]; then
+	if [[ $span -ge 366 ]]; then
 	echo "Installing/updating R packages.
 	"
 	echo "Installing/updating R packages." >> $log
@@ -361,9 +361,9 @@ source /etc/environment 1>$stdout 2>$stderr || true
 	wait
 	echo $Rdate1 > $scriptdir/updates/R_installs_and_updates.txt
 	else
-	echo "R installs/updates have been run within the past month.  Skipping.
+	echo "R installs/updates have been run within the past year.  Skipping.
 	"
-	echo "R installs/updates have been run within the past month.  Skipping.
+	echo "R installs/updates have been run within the past year.  Skipping.
 	" >> $log
 	fi
 
